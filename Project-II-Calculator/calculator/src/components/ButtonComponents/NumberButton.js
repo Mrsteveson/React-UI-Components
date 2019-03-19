0c9f9bb264461582afd66f861926638a486d9079
mrsteveson <patrick.steveson727@gmail.com>
2019-03-19 17:12:04 -0500

    mvp done hopefully

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props){
+    return (
+        <button className={props.button.buttonStyle}>
+            {props.button.text}
+        </button>
+    );
+}
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+function CalcDisplay(props){
+    return(
+        <div className = "calc-display">
+            <h2>{props.currentValue}</h2>
+        </div>
+    );
+}
+
+export default CalcDisplay;
